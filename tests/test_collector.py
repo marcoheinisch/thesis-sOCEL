6a98c730099b1e1d1088acd497c2f484647e8dc8
marcoheinisch <57726217+marcoheinisch@users.noreply.github.com>
2024-03-25 18:24:23 +0100

    move tests

---
+++ tests/test_collector.py
@@ -0,0 +1,49 @@
+import main # enables logging while testing
+
+import unittest
+import os
+import logging
+
+import cpn_api.collector as collector
+import cpn_api.configurator as xml
+import config
+
+
+class TestCollector(unittest.TestCase):
+
+    def setUp(self):
+        self.request_id = 'id_electricity-supply_grid-source_nuclear'
+        self.quantity_name = 'energy'
+        self.param_data = {
+            "energy": 100,
+            "energy_unit": "kWh"
+        }
+        self.factor_data = {
+            "id": 'f4eeeece-ad93-47c6-b216-b3f01d24afb6'
+        }
+        xml.add_request_climatiq(
+            self.request_id, 
+            self.param_data, 
+            self.factor_data, 
+            self.quantity_name
+        )
+        self.json_body = {
+            "parameters": self.param_data,
+            "emission_factor": self.factor_data
+        }
+        self.co2e = 5.5
+
+    def test_get_co2e_by_climatiq_call(self):
+        self.assertEqual(xml.check_request_entry(self.request_id), True)
+        co2e = collector.get_co2e_by_climatiq_call(
+            url="https://beta4.api.climatiq.io/estimate",
+            json_body=self.json_body
+        )
+        self.assertEqual(co2e, self.co2e)
+
+    def test_get_co2e(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_configurator.py
@@ -0,0 +1,47 @@
+import main # enables logging while testing
+
+import unittest
+import os
+import logging
+
+import cpn_api.configurator as xml
+import config
+
+
+class TestConfigurator(unittest.TestCase):
+
+    def setUp(self):
+        self.param_data = {
+            "weight": 1,
+            "weight_unit": "t"
+        }
+        self.factor_data = {
+            "activity_id": "metals-type_steel_cold_rolled_coil",
+            "region": "GLOBAL",
+            "data_version": "^3"
+        }
+        self.quantity_name = "weight"
+        self.exisiting_id = "id_example_call_existing"
+        self.not_exisiting_id = "id_example_call_nooooot_existing"
+        xml.clear_requests()
+
+    def test_xml_write(self):
+        xml.clear_requests()
+        xml.add_request_climatiq(self.exisiting_id, self.param_data, self.factor_data, self.quantity_name)
+
+    def test_xml_read_id(self):
+        xml.add_request_climatiq(self.exisiting_id, self.param_data, self.factor_data, self.quantity_name)
+        endpoint, json_body, quantity_name = xml.get_request_entry(self.exisiting_id)
+        for key, value in self.param_data.items():
+            self.assertEqual(json_body["parameters"][key], value)
+        for key, value in self.factor_data.items():
+            self.assertEqual(json_body["emission_factor"][key], value)
+
+    def test_xml_read_no_id(self):
+        self.assertEqual(xml.check_request_entry(self.not_exisiting_id), False)
+        with self.assertRaises(ValueError):
+            xml.get_request_entry(self.not_exisiting_id)
+
+
+if __name__ == '__main__':
+    unittest.main()
